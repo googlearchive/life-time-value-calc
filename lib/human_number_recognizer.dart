9e1ba024342cea0cc8b9c481fbbc5059d3fba8b0
Filip Hracek <filiph@google.com>
2012-11-20 17:39:15 +0100

    Fix breaking change with RegExp (no const constructor)

--- lib/human_number_recognizer.dart
+++ lib/human_number_recognizer.dart
@@ -4,29 +4,29 @@ import "dart:math";
 
 class HumanNumber {
   // TODO: have state = setup "locale", "learn", etc.
-  
+
   // Ex: -1 165 849.918
-  static RegExp validEnglishLargeNumber = 
-      const RegExp(r"^\s*([-+]?\s*([0-9]{1,3}[, ])+[0-9]{3}(\.?[0-9]+)*)\s*$");
+  static RegExp validEnglishLargeNumber =
+      new RegExp(r"^\s*([-+]?\s*([0-9]{1,3}[, ])+[0-9]{3}(\.?[0-9]+)*)\s*$");
   // Ex: -1.165.849,918
-  static RegExp validCzechLargeNumber = 
-      const RegExp(r"^\s*([-+]?\s*([0-9]{1,3}[. ])+[0-9]{3}(\,?[0-9]+)*)\s*$");
+  static RegExp validCzechLargeNumber =
+      new RegExp(r"^\s*([-+]?\s*([0-9]{1,3}[. ])+[0-9]{3}(\,?[0-9]+)*)\s*$");
   // Ex: 1,400
   static RegExp thousandLookingNumber =
-      const RegExp(r"^\s*([-+]?\s*[0-9]{1,3}[,\. ][0-9]{2}0)\s*$");
+      new RegExp(r"^\s*([-+]?\s*[0-9]{1,3}[,\. ][0-9]{2}0)\s*$");
   // Ex: 15616.8, but also "489,48984,,,6.46,"
-  static RegExp numberCharsString = 
-      const RegExp(r"^\s*([-+]?\s*[0-9,\. ]*[0-9])\s*$");
-  static RegExp numberCharsStringWithEndingDot = 
-      const RegExp(r"^\s*([-+]?\s*[0-9,\. ]*[0-9])[\.,]\s*$");
-  static RegExp numberCharsStringWithEndingPercentage = 
-      const RegExp(r"^\s*([-+]?\s*[0-9,\. ]*[0-9])[\.,]?\s*%\s*$");
-  
-  static RegExp whiteSpace = const RegExp(r"\s");
-  static RegExp commaOrDot = const RegExp(r"[\.,]");
-  
+  static RegExp numberCharsString =
+      new RegExp(r"^\s*([-+]?\s*[0-9,\. ]*[0-9])\s*$");
+  static RegExp numberCharsStringWithEndingDot =
+      new RegExp(r"^\s*([-+]?\s*[0-9,\. ]*[0-9])[\.,]\s*$");
+  static RegExp numberCharsStringWithEndingPercentage =
+      new RegExp(r"^\s*([-+]?\s*[0-9,\. ]*[0-9])[\.,]?\s*%\s*$");
+
+  static RegExp whiteSpace = new RegExp(r"\s");
+  static RegExp commaOrDot = new RegExp(r"[\.,]");
+
   static num recognizeString(String s) {
-    
+
     if (!numberCharsString.hasMatch(s)) {
       if (numberCharsStringWithEndingDot.hasMatch(s)) {
         s = numberCharsStringWithEndingDot.firstMatch(s).group(1);
@@ -35,8 +35,8 @@ class HumanNumber {
       } else {
         return null;
       }
-    } 
-  
+    }
+
     int numberOfDots = 0;
     int numberOfCommas = 0;
     for (int i = 0; i < s.length; i++) {
@@ -45,7 +45,7 @@ class HumanNumber {
       else if (s[i] == ",")
         numberOfCommas++;
     }
-        
+
     if ((numberOfDots > 0 && numberOfCommas > 0) || (numberOfDots > 1 || numberOfCommas > 1)) {
       if (validEnglishLargeNumber.hasMatch(s)) {
         s = s.replaceAll(",", "");
@@ -58,20 +58,20 @@ class HumanNumber {
     } else if (thousandLookingNumber.hasMatch(s)) {
       s = s.replaceAll(commaOrDot, "");
     } else if (numberOfCommas == 1) {
-      // TODO this would benefit from context - if all other numbers are english, a "1,234" probably means 1234 
+      // TODO this would benefit from context - if all other numbers are english, a "1,234" probably means 1234
       // assuming czech format
       s = s.replaceAll(",", ".");
     }
-    
+
     num result = null;
-    
+
     try {
       result = double.parse(s.replaceAll(whiteSpace, ""));
     } on FormatException catch (e) {
       print("Format Exception for string '$s': $e");
     }
-    
+
     return result;
-  
+
   }
 }
